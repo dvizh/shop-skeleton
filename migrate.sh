b3d75d5ec4153d5fd98933c184f4b520c3bcc036
Hal <halumein@gmail.com>
2017-04-28 19:14:16 +0500

    Create migrate.sh
    баш-скрипт для выполнения всех init-миграций разом, без подтверждения.

---
+++ migrate.sh
@@ -0,0 +1,12 @@
+php yii migrate/up --migrationPath=vendor/yiisoft/yii2/rbac/migrations --interactive=0
+php yii migrate/up --migrationPath=common/modules/dektrium/yii2-user/migrations --interactive=0
+php yii migrate/up --migrationPath=migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-shop/src/migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-order/src/migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-cart/src/migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-filter/src/migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-field/src/migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-seo/src/migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-gallery/src/migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-promocode/src/migrations --interactive=0
+php yii migrate/up --migrationPath=vendor/dvizh/yii2-certificate/src/migrations --interactive=0
