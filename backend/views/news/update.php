3df1203d4fb6bc52ad0f17cfda42cf26b188cd4c
Sergey <PROloggg@yandex.ru>
2017-07-06 14:39:05 +0500

    Update update.php

--- backend/views/news/update.php
+++ backend/views/news/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\News */
 
-$this->title = 'Редактировать Новость: ' . $model->title;
+$this->title = 'Редактировать Новость: ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Новости', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Редактировать';
 ?>
 <div class="news-update">
